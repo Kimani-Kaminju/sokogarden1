c6f9e8cddc7071d6814e187464a4dcda78ebd87b
Kimani-Kaminju <lansonkimani@gmail.com>
2026-06-26 14:31:31 +0300

    Added

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ def signup():
         #create a cursor
         cursor = connection.cursor()
 
+        # Check if email already exists
+        check_sql = "SELECT * FROM users WHERE email = %s"
+        cursor.execute(check_sql, (email,))
+        user = cursor.fetchone()
+        if user:
+            return jsonify({"message": "user already registered"})
+
         #structure the sql query for insert
         # %s-pre-prepared statement.It is a placeholder that will replace actual value
         sql = "insert into users(username,password,email,phone) values (%s,%s,%s,%s)"
@@ -36,8 +43,40 @@ def signup():
         connection.commit()
 
         #give a response to the user
-        return jsonify({"message" : "User registerd successfully"})
+        return jsonify({"message" : "User registered successfully"})
+#import pymysql cursor
+import pymysql.cursors
+#below is the sign in api end point
+@app.route("/api/signin",methods=["POST"])  
+def signin():
+    if request.method == "POST":
+        #extract the dta from post man
+        email = request.form["email"]
+        password = request.form["password"]
 
+        #create a connection to db
+        connection = pymysql.connect(host="localhost",password="",user="root",database="sokogarden")
+
+        #create cursor
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+        #structure the sql query to check whether the person trying to login already has an account
+        sql = "select * from users where email = %s and password = %s"
+
+        #tuple
+        data = (email , password)
+        #cursor,execute the query
+        cursor.execute(sql,data)
+
+        #check how many rows are returned when the query is executed
+        count = cursor.rowcount
+
+        if count == 0:
+            return jsonify({"message": "Login failed.Please check on your details enterer"})   
+        else:
+            #if the user is there, take the details of the user and store them onto a variable and return a message of success
+            user = cursor.fetchone()
+            return jsonify({"message" : "Login success" , "user": user})
 
 
 #Run
